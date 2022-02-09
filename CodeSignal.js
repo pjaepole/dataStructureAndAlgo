2ce171e14e9da5e74191290a723e3c6b14f15726
Jaehun ParkLaptop <pjaehun4122@gmail.com>
2022-02-09 13:05:52 -0800

    swap first and last element in an array

--- CodeSignal.js
+++ CodeSignal.js
@@ -47,3 +47,29 @@ function solution(inputArray, elemToReplace, substitutionElem) {
     return inputArray.map(i=> i==elemToReplace ? substitutionElem : i)}
 //what this person did was used map array function to map through the array and if i the element insdie the array is equal to elemToReplace
 //statement is true then it replaces it with substitutionElem if i==elemToReplace is false it leave it be 
+
+
+// Problem first reverse try
+// Reversing an array can be a tough task, especially for a novice programmer. Mary just started coding, so she would like to start with something basic at first. Instead of reversing the array entirely, she wants to swap just its first and last elements.
+// Given an array arr, swap its first and last elements and return the resulting array.
+// Example
+// For arr = [1, 2, 3, 4, 5], the output should be
+// solution(arr) = [5, 2, 3, 4, 1].
+// Input/Output
+// [execution time limit] 4 seconds (js)
+// [input] array.integer arr
+// Guaranteed constraints:
+// 0 ≤ arr.length ≤ 50,
+// -104 ≤ arr[i] ≤ 104.
+// [output] array.integer
+// Array arr with its first and its last elements swapped.
+function solution(arr) {
+    if(arr.length==0){
+        return arr
+    }
+    let arrHead=arr[0]
+    let arrTail=arr[arr.length-1]
+    arr[0]=arrTail
+    arr[arr.length-1]=arrHead
+    return arr
+}
