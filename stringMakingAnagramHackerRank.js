02406f15f8db12ca173c731f379fc51f2ae82520
Jaehun ParkLaptop <pjaehun4122@gmail.com>
2022-02-21 14:29:06 -0800

    explanation added

--- stringMakingAnagramHackerRank.js
+++ stringMakingAnagramHackerRank.js
@@ -46,16 +46,16 @@ It takes  deletions to make both strings anagrams. */
 
 
 function makeAnagram(a, b) {
-    let counter=0
-    let tracker={}
-    for(let i=0;i<a.length;i++){
+    let counter=0 //will keep track of how many deletion from each a and b param
+    let tracker={} //will track both a and b and how many letters are present inside.
+    for(let i=0;i<a.length;i++){ //loop through and create obj, for a I will add 1 each time letter is present
         if(!tracker[a[i]]){
             tracker[a[i]]=1
         } else if(tracker[a[i]]){
             tracker[a[i]]++
         }
     }
-    for(let i=0;i<b.length;i++){
+    for(let i=0;i<b.length;i++){//loop through and create obj, for b I will subtract 1 each time letter is present
         if(!tracker[b[i]]){
             tracker[b[i]]=-1
         } else {
@@ -63,8 +63,8 @@ function makeAnagram(a, b) {
         }
     }
     for(let letter in tracker){
-        if(tracker[letter]!==0){
-            counter+=Math.abs(tracker[letter])
+        if(tracker[letter]!==0){ //so when obj value is not 0 it means from either a or b they have letter that is outnumbering(not need to make anagram)
+            counter+=Math.abs(tracker[letter]) //and since for b is negative number I use the Math.abs to make it into positive integer and add it to counter
         }
     }
     return counter
